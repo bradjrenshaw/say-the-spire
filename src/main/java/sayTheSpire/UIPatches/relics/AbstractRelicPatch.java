4d4a640bb007f75475c50f0338be7324f10bb772
bradjrenshaw <bradjrenshaw@gmail.com>
2021-11-29 22:23:40 -0500

    Fix relics being repeated as diferent element types in the compendium.

--- src/main/java/sayTheSpire/UIPatches/relics/AbstractRelicPatch.java
+++ src/main/java/sayTheSpire/UIPatches/relics/AbstractRelicPatch.java
@@ -1,7 +1,9 @@
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.screens.mainMenu.MainMenuScreen;
 import sayTheSpire.ui.elements.RelicElement;
 import sayTheSpire.Output;
 import sayTheSpire.utils.RelicUtils;
@@ -14,6 +16,8 @@ public class AbstractRelicPatch {
         if (__instance.hb.justHovered) {
             AbstractPlayer player = OutputUtils.getPlayer();
             RelicElement.RelicLocation location = RelicElement.RelicLocation.OTHER;
+            if (CardCrawlGame.mainMenuScreen.screen == MainMenuScreen.CurScreen.RELIC_VIEW)
+                return;
             if (player != null && player.relics.indexOf(__instance) >= 0) {
                 location = RelicElement.RelicLocation.MAIN_SCREEN;
             }
