65f29564f995de655fd0825e4cd0d37538c69529
bradjrenshaw <bradjrenshaw@gmail.com>
2021-11-29 23:13:24 -0500

    Add viewing the current ascension level and associated modifiers in game.

--- src/main/java/sayTheSpire/UIPatches/ui/panels/TopPanelPatch.java
+++ src/main/java/sayTheSpire/UIPatches/ui/panels/TopPanelPatch.java
@@ -1,13 +1,16 @@
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import basemod.ReflectionHacks;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
 import com.megacrit.cardcrawl.potions.PotionSlot;
+import com.megacrit.cardcrawl.screens.charSelect.CharacterSelectScreen;
 import com.megacrit.cardcrawl.ui.panels.TopPanel;
 import sayTheSpire.ui.elements.PotionElement;
 import sayTheSpire.ui.elements.ResourceElement;
 import sayTheSpire.utils.OutputUtils;
 import sayTheSpire.Output;
+import sayTheSpire.TextParser;
 
 @SpirePatch(clz = TopPanel.class, method = "updatePotions")
 public class TopPanelPatch {
@@ -23,6 +26,10 @@ public class TopPanelPatch {
             Output.setUI(new ResourceElement(TopPanel.LABEL[3], TopPanel.MSG[3],
                     player.currentHealth + "/" + player.maxHealth));
             return true;
+        } else if (__instance.ascensionHb.justHovered) {
+            String ascensionString = (String) ReflectionHacks.getPrivate(__instance, TopPanel.class, "ascensionString");
+            Output.setUI(new ResourceElement(CharacterSelectScreen.TEXT[8], TextParser.parse(ascensionString),
+                    String.valueOf(AbstractDungeon.ascensionLevel)));
         }
         return false;
     }
