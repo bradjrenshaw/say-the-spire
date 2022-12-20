8de47875d156e901dfbb04c5b209643a419a7864
bradjrenshaw <bradjrenshaw@gmail.com>
2022-12-20 18:25:17 -0500

    Localize relic compendium screen.

--- src/main/java/sayTheSpire/UIPatches/screens/compendium/RelicViewScreenPatch.java
+++ src/main/java/sayTheSpire/UIPatches/screens/compendium/RelicViewScreenPatch.java
@@ -3,6 +3,7 @@ import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.screens.compendium.RelicViewScreen;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import basemod.ReflectionHacks;
+import sayTheSpire.localization.LocalizationContext;
 import sayTheSpire.ui.positions.CategoryListPosition;
 import sayTheSpire.ui.elements.RelicElement;
 import sayTheSpire.Output;
@@ -18,9 +19,17 @@ public class RelicViewScreenPatch {
             if (hoveredRelic != null && hoveredRelic.hb.justHovered) {
                 ArrayList<AbstractRelic> relicGroup = (ArrayList<AbstractRelic>) ReflectionHacks.getPrivate(__instance,
                         RelicViewScreen.class, "relicGroup");
-                String category = hoveredRelic.tier.name().toLowerCase() + " relics";
-                RelicElement element = new RelicElement(hoveredRelic, RelicElement.RelicLocation.COMPENDIUM,
-                        new CategoryListPosition(relicGroup.indexOf(hoveredRelic), relicGroup.size(), category));
+                LocalizationContext localization = Output.localization.getContext("ui.screens.RelicViewScreen");
+                String key = hoveredRelic.tier.name().toLowerCase();
+                CategoryListPosition position = null;
+                String localizedType = localization.localize("types." + key);
+                if (localizedType == null)
+                    localizedType = key;
+                localization.put("category", localizedType);
+                String category = localization.localize("categoryLabel");
+                if (category != null)
+                    position = new CategoryListPosition(relicGroup.indexOf(hoveredRelic), relicGroup.size(), category);
+                RelicElement element = new RelicElement(hoveredRelic, RelicElement.RelicLocation.COMPENDIUM, position);
                 Output.setUI(element);
             }
         }
