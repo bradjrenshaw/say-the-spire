6ec47fd5637f4dd1291ebe2e16d377ec09f1de11
bradjrenshaw <bradjrenshaw@gmail.com>
2022-12-30 22:51:30 -0500

    Updated version strings.

--- src/main/java/sayTheSpire/Output.java
+++ src/main/java/sayTheSpire/Output.java
@@ -30,7 +30,7 @@ import sayTheSpire.buffers.*;
 
 public class Output {
 
-    public static String modVersion = "0.4.0-beta";
+    public static String modVersion = "0.4.1-beta";
 
     private static Logger logger = LogManager.getLogger(Output.class.getName());
 
