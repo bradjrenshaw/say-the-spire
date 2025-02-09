21cccf318d92e9543af51ee41ad7c2e65941f00c
druidah <infrapocok@gmail.com>
2025-02-09 10:58:32 +0100

    Combat is fully supported now as far as i can tell. Needs further testing though.

--- src/main/java/sayTheSpire/buffers/MonsterBuffer.java
+++ src/main/java/sayTheSpire/buffers/MonsterBuffer.java
@@ -20,6 +20,10 @@ import sayTheSpire.utils.OutputUtils;
 import sayTheSpire.TextParser;
 import sayTheSpire.ui.elements.MonsterElement;
 
+//downfall specific imports so we don'tget random numbers and stuff like that in the middle of the descriptions
+import sayTheSpire.utils.CardUtils;
+import sayTheSpire.utils.RelicUtils;
+
 public class MonsterBuffer extends Buffer {
 
     private MonsterElement monster;
@@ -46,7 +50,7 @@ public class MonsterBuffer extends Buffer {
         this.context.put("hp", this.monster.getCurrentHealth());
         this.context.put("hpMax", this.monster.getMaxHealth());
         this.context.put("block", this.monster.getCurrentBlock());
-        this.add("xxx" + this.monster.getName() + "neve:");
+        this.add(this.monster.getName());
         this.addLocalized("content.hp");
         this.addLocalized("content.block");
         this.add(this.monster.getPowersString());
@@ -55,40 +59,40 @@ public class MonsterBuffer extends Buffer {
             this.add(TextParser.parse(tip.header + " NL " + tip.body));
         }
 
-        // Downfall mod támogatás
+        // Downfall specific
         if (Loader.isModLoaded("downfall") && EvilModeCharacterSelect.evilMode
                 && this.monster.getMonster() instanceof AbstractCharBoss) {
             AbstractCharBoss boss = (AbstractCharBoss) this.monster.getMonster();
-            this.add("Hand:");
-            for (AbstractCard card : boss.hand.group) {
-                this.add(" - " + card.name);
-            }
             this.add("Energy: " + EnemyEnergyPanel.totalCount);
 
+            if (boss.stance instanceof AbstractEnemyStance) {
+                this.add("Stance: " + ((AbstractEnemyStance) boss.stance).ID);
+            }
+            this.add("Hand:");
+            for (AbstractCard card : boss.hand.group) {
+                this.add(" - " + card.name + "," + card.cost + "," + CardUtils.getCardDescriptionString(card));
+            }
             if (!boss.orbs.isEmpty()) {
                 this.add("Orbs:");
                 for (AbstractOrb orb : boss.orbs) {
                     if (orb instanceof Dark) {
                         this.add(" - Dark Orb (" + orb.evokeAmount + ")");
                     } else {
-                        this.add(" - " + orb.name);
+                        this.add(" - " + orb.name + "," + orb.evokeAmount);
                     }
                 }
             }
-
-            if (boss.stance instanceof AbstractEnemyStance) {
-                this.add("Stance: " + ((AbstractEnemyStance) boss.stance).ID);
-            }
-
             if (!boss.relics.isEmpty()) {
                 this.add("Relics:");
                 for (AbstractRelic relic : boss.relics) {
                     if (relic.counter >= 0) {
-                        this.add(" - " + relic.name + " (" + relic.counter + ")");
+                        this.add(
+                                " - " + relic.name + "," + relic.counter + "," + RelicUtils.getRelicDescription(relic));
                     } else {
-                        this.add(" - " + relic.name);
+                        this.add(" - " + relic.name + "," + RelicUtils.getRelicDescription(relic));
                     }
                 }
+
             }
         }
     }
